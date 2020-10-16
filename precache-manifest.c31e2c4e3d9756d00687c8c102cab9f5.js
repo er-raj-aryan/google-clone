152564c19d1df122a2038439d53c0b14615c30f4
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2020-10-16 07:40:56 +0530

    Google-Clone

---
+++ precache-manifest.c31e2c4e3d9756d00687c8c102cab9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2897416adee9ca18802b7650762e4e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abff5d3465a4a6d046cd",
+    "url": "/static/css/main.8e979065.chunk.css"
+  },
+  {
+    "revision": "98b556ec2e777db95c0d",
+    "url": "/static/js/2.abbbde5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.abbbde5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abff5d3465a4a6d046cd",
+    "url": "/static/js/main.0d883ce5.chunk.js"
+  },
+  {
+    "revision": "2c4629e9161e8a7ceef0",
+    "url": "/static/js/runtime-main.74063fa0.js"
+  }
+]);
